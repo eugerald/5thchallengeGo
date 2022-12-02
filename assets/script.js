a8a875a9c21bce4bda66369584dcfc0f575bd228
James <eugerald@email.com>
2022-12-02 13:32:55 -0600

    Addng color change

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,33 @@ $(".saveplan").on("click",function(){
     var time = $(this).siblings("textarea").attr("id")
     localStorage.setItem(time,userPlanEntry)
 })
+// $("#11").val(localStorage.getItem("11"));
+// $("#10").val(localStorage.getItem("10"));
+// $("#9").val(localStorage.getItem("9"));
+// $("#12").val(localStorage.getItem("12"));
+// $("#13").val(localStorage.getItem("13"));
+// $("#14").val(localStorage.getItem("14"));
+// $("#15").val(localStorage.getItem("15"));
+// $("#16").val(localStorage.getItem("16"));
+
+var currentHour = dayjs().hour()
+
+for(let i =9;i<=17;i++){
+    $("#"+i).val(localStorage.getItem(i));
+    if(i<currentHour){
+        $("#"+i).addClass("bg-secondary")
+    }else if(currentHour === i){
+        $("#"+i).addClass("bg-danger")
+
+    }else{    
+            $("#"+i).addClass("bg-info")
+
+
+    }
+
+}
+
+
+
+$("#date").text(dayjs().format("MM/DD/YYYY"))
+
